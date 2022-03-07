25f52e1511038cba3ca18731521a091961d9637c
Tom Hepworth <tom@Thmbp21.local>
2022-03-07 17:36:31 +0000

    working python example

--- python/main.py
+++ python/main.py
@@ -1,12 +1,29 @@
 import pyopenvdb as vdb
+import math
 
-grid = vdb.FloatGrid()
-grid.name = 'density'
-accessor = grid.getAccessor()
+exact = vdb.FloatGrid()
+exact.name = 'exact'
+exactAccessor = exact.getAccessor()
 
-for i in range(-5,6):
-    for j in range(-5,6):
-        for k in range(-5,6):
-            accessor.setValueOn((i,j,k), 0.125)
+paraxial = vdb.FloatGrid()
+paraxial.name = 'paraxial'
+paraxialAccessor = paraxial.getAccessor()
 
-vdb.write('mygrid.vdb', grids=[grid])
+
+for x in range(-400,400):
+    for y in range(0,1):
+        for z in range(-300,700):
+            if (z == 0):
+                continue
+            rho2 = x**2 + y**2
+            r2 = rho2 + z**2
+            r = math.sqrt(r2)
+            rho = math.sqrt(rho2)
+            
+            exactPhase = math.cos(r/15)
+            paraxialPhase = math.cos((z+(rho2)/(2*z))/15)
+            
+            exactAccessor.setValueOn((x,y,z), exactPhase)
+            paraxialAccessor.setValueOn((x,y,z), paraxialPhase)
+
+vdb.write('optics.vdb', grids=[exact, paraxial])
